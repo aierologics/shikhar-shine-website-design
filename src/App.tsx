0f0ab798f9f5110d3d027c0463528bd6db005c79
Garvit Rajput <garvit.rajput@aierologics.com>
2025-07-11 09:04:35 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { navItems } from "./nav-items";
-import Index from "./pages/Index";
 import AdminLayout from "./components/AdminLayout";
-import AdminPanel from "./pages/AdminPanel";
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminStudents from "./pages/AdminStudents";
 import AdminTeachers from "./pages/AdminTeachers";
@@ -23,6 +20,21 @@ import AdminAdmissions from "./pages/AdminAdmissions";
 import AdminNotices from "./pages/AdminNotices";
 import AdminGallery from "./pages/AdminGallery";
 import AdminSettings from "./pages/AdminSettings";
+import FeeDetails from "./pages/FeeDetails";
+import { Contact } from "lucide-react";
+import About from "./pages/About";
+import AdmissionDocuments from "./pages/AdmissionDocuments";
+import Admissions from "./pages/Admissions";
+import Auth from "./pages/Auth";
+import Facilities from "./pages/Facilities";
+import Gallery from "./pages/Gallery";
+import Index from "./pages/Index";
+import MandatoryPublicDisclosure from "./pages/MandatoryPublicDisclosure";
+import Programs from "./pages/Programs";
+import StaffDetails from "./pages/StaffDetails";
+import TCVerification from "./pages/TCVerification";
+import ContactSection from "./components/ContactSection";
+import AdminFees from "./pages/AdminFees";
 
 const queryClient = new QueryClient();
 
@@ -33,11 +45,19 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          {navItems.map(({ to, page }) => (
-            <Route key={to} path={to} element={page} />
-          ))}
-          <Route path="/" element={<Index />} />
-          
+         <Route path="/" element={<Index />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<ContactSection />} />
+            <Route path="/programs" element={<Programs />} />
+            <Route path="/facilities" element={<Facilities />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/admissions" element={<Admissions />} />
+            <Route path="/fee-details" element={<FeeDetails />} />
+            <Route path="/tc-verification" element={<TCVerification />} />
+            <Route path="/mandatory-public-disclosure" element={<MandatoryPublicDisclosure />} />
+            <Route path="/staff-details" element={<StaffDetails />} />
+            <Route path="/admission-documents" element={<AdmissionDocuments />} />
+            <Route path="/auth" element={<Auth />} />
           {/* Admin Routes */}
           <Route path="/admin" element={<AdminLayout />}>
             <Route index element={<AdminDashboard />} />
@@ -46,7 +66,7 @@ const App = () => (
             <Route path="teachers" element={<AdminTeachers />} />
             <Route path="classes" element={<AdminClasses />} />
             <Route path="timetable" element={<AdminTimetable />} />
-            <Route path="fees" element={<div>Fee Structure Management - Coming Soon</div>} />
+            <Route path="fees" element={<AdminFees/>} />
             <Route path="fee-deposits" element={<AdminFeeDeposits />} />
             <Route path="hostels" element={<AdminHostels />} />
             <Route path="transport" element={<AdminTransport />} />
